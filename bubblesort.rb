3b3a93c4f9d80b422102144905b5ec675dacb1f7
Marco <marcoerhardt95@gmail.com>
2020-09-17 14:50:44 +0200

    did some tests, added end

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,14 +1,16 @@
 def bubble_sort(arr)
   my_arr = arr
   
-  (0...arr.length).each { |i|
+  (0...arr.length).each do |i|
     if my_arr[i] < my_arr[i+1]
       next
     else
       temp = my_arr[i]
       my_arr[i] = my_arr[i+1]
       my_arr[i+1] = temp
-  }
+    end
 
-my_arr
+  puts my_arr
+  end
 end
+bubble_sort([1, 5, 2, 3, 1])
