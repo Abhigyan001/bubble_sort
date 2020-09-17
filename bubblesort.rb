bc84806b8f7e93cccb888110a90a9adada8cc942
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-09-17 20:49:44 +0530

    fixed linter errors

--- bubblesort.rb
+++ bubblesort.rb
@@ -6,19 +6,15 @@ def bubble_sort(arr)
   while condition
     condition = false
     (0...arr.length - 1).each do |i|
-      if (my_arr[i] < my_arr[i + 1])
-        next
-      else
-        temp = my_arr[i]
-        my_arr[i] = my_arr[i + 1]
-        my_arr[i + 1] = temp
-        condition = true
-      end  
+      next unless my_arr[i] > my_arr[i + 1]
+      temp = my_arr[i]
+      my_arr[i] = my_arr[i + 1]
+      my_arr[i + 1] = temp
+      condition = true
     end
   end
-  my_arr
+my_arr
 end
-
 
 # bubble sort with usage of yield
 
@@ -28,19 +24,16 @@ def bubble_sort_by(arr)
   while condition
     condition = false
     (0...arr.length - 1).each do |i|
-      if yield(my_arr[i], my_arr[i + 1]).negative?  
-        next    
-      else
-        temp = my_arr[i]
-        my_arr[i] = my_arr[i + 1]
-        my_arr[i + 1] = temp
-        condition = true
-      end
+      next unless yield(my_arr[i], my_arr[i + 1]).positive?
+      temp = my_arr[i]
+      my_arr[i] = my_arr[i + 1]
+      my_arr[i + 1] = temp
+      condition = true      
     end
   end
   puts my_arr
 end
 
-bubble_sort_by(%w[bla, ba, blabla, b, blaaablaa]) do |left, right|
+bubble_sort_by(%w[bla ba blabla b blaaablaa]) do |left, right|
   left.length - right.length
 end
