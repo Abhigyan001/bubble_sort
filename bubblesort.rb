6e451cdb57a1875233408eb4501fedeaaf60e9b4
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-09-17 20:53:08 +0530

    Final fixes done

--- bubblesort.rb
+++ bubblesort.rb
@@ -7,13 +7,14 @@ def bubble_sort(arr)
     condition = false
     (0...arr.length - 1).each do |i|
       next unless my_arr[i] > my_arr[i + 1]
+
       temp = my_arr[i]
       my_arr[i] = my_arr[i + 1]
       my_arr[i + 1] = temp
       condition = true
     end
   end
-my_arr
+  my_arr
 end
 
 # bubble sort with usage of yield
@@ -25,10 +26,11 @@ def bubble_sort_by(arr)
     condition = false
     (0...arr.length - 1).each do |i|
       next unless yield(my_arr[i], my_arr[i + 1]).positive?
+
       temp = my_arr[i]
       my_arr[i] = my_arr[i + 1]
       my_arr[i + 1] = temp
-      condition = true      
+      condition = true
     end
   end
   puts my_arr
