270fcf5ceb4737f04be8e5fdae76a680ce76bbb6
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-09-17 18:48:32 +0530

    Fix bubblesort using while loop

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,16 +1,19 @@
 def bubble_sort(arr)
   my_arr = arr
-  
-  (0...arr.length).each do |i|
-    if my_arr[i] < my_arr[i+1]
-      next
-    else
-      temp = my_arr[i]
-      my_arr[i] = my_arr[i+1]
-      my_arr[i+1] = temp
+  condition = true
+  while condition
+    condition = false
+    (0...arr.length-1).each do |i|
+      if my_arr[i] < my_arr[i+1]
+        next
+      else
+        temp = my_arr[i]
+        my_arr[i] = my_arr[i+1]
+        my_arr[i+1] = temp
+        condition = true
+      end  
     end
-
-  puts my_arr
   end
+  puts my_arr
 end
-bubble_sort([1, 5, 2, 3, 1])
+bubble_sort([1, 56, 34, 12, 0])
