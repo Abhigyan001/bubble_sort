93cabbe02f470111565a94876f03009afd7a04d4
Marco <marcoerhardt95@gmail.com>
2020-09-17 16:10:23 +0200

    finish bubble-sort-by method, add comments

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,3 +1,5 @@
+#default bubble sort
+
 def bubble_sort(arr)
   my_arr = arr
   condition = true
@@ -14,12 +16,11 @@ def bubble_sort(arr)
       end  
     end
   end
-  puts my_arr
+  my_arr
 end
 
 
-
-
+#bubble sort with usage of yield
 
 def bubble_sort_by(arr)
   my_arr = arr
@@ -27,19 +28,19 @@ def bubble_sort_by(arr)
   while condition
     condition = false
     (0...arr.length-1).each do |i|
-      yield(my_arr[i], my_arr[i+1]).positive?
+      if yield(my_arr[i], my_arr[i+1]).negative?
         next
-      
+      else
         temp = my_arr[i]
         my_arr[i] = my_arr[i+1]
         my_arr[i+1] = temp
         condition = true
-      
+      end
     end
   end
   puts my_arr
 end
 
-bubble_sort_by([2, 5, 1, 9, 6]) do |left,right|
-  left - right
+bubble_sort_by(["bla", "ba", "blabla", "b", "blaaablaa"]) do |left,right|
+  left.length - right.length
 end
